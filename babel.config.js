f78f98c683cd3c2baad0dbb7993370d5aa242170
Mario Stefanutti <mario.stefanutti@gmail.com>
2025-08-05 23:25:37 +0200

    configure babel for commonjs helpers

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    // Allow Babel to automatically determine whether a file is an ES module
+    // or CommonJS. This prevents CommonJS helpers in node_modules from being
+    // transformed into ESM default exports, which can cause runtime errors
+    // such as "_interopRequireDefault is not a function" on web.
+    sourceType: 'unambiguous',
     plugins: [],
   };
 };
