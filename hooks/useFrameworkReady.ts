4e6babd3b0a2cb17c32d0ec174d297e29a8a3c83
Mario Stefanutti <mario.stefanutti@gmail.com>
2025-08-05 23:14:30 +0200

    fix: remove metro config from framework hook

--- hooks/useFrameworkReady.ts
+++ hooks/useFrameworkReady.ts
@@ -1,25 +1,11 @@
 import { useEffect } from 'react';
 import { Platform } from 'react-native';
-const { getDefaultConfig } = require('expo/metro-config');
 
 declare global {
   interface Window {
     frameworkReady?: () => void;
   }
 }
-
-const config = getDefaultConfig(__dirname);
-
-// Add web support
-config.resolver.platforms = ['ios', 'android', 'web'];
-
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [],
-  };
-};
 
 export function useFrameworkReady() {
   useEffect(() => {
